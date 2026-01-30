7d267477bc66744a2384d23b08dfde0dc4f6a758
Eric Gu <eric.changning.gu@gmail.com>
2026-01-30 08:48:03 -0500

    feat: Change API Name

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.openapi.docs import get_swagger_ui_html
 
 app = FastAPI(
-    title="Juniper Mist Mega Lab API",
+    title="Juniper Mist - Multi Site Provisioning Service API",
     version="1.0.0"
 )
+
+@app.get("/docs", include_in_schema=False)  # Hide from OpenAPI schema
+def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI"
+    )
 
 @app.get("/")
 def redirect_to_docs():
