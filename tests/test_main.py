9555ccc2800113f8d0dd36d2e6dd04fc151df79e
Eric Gu <eric.changning.gu@gmail.com>
2026-01-30 08:42:06 -0500

    fix: Remove Root Test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,6 @@
 from fastapi.testclient import TestClient
 from src.main import app
 
-def test_root():
-    client = TestClient(app)
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
-
 def test_status():
     client = TestClient(app)
     response = client.get("/status")
